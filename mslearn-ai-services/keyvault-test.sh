724ed120381a885cefd7066e24fe7dd8cd7c88d2
Liew Chooi Chin <127520432+liewchooichin@users.noreply.github.com>
2024-05-14 15:24:06 +0000

    use secrets

--- mslearn-ai-services/keyvault-test.sh
+++ mslearn-ai-services/keyvault-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mykey=4a44f2f5ab424bbbbe46354043889fa1
 myendpoint=https://lcclab002.cognitiveservices.azure.com/
-curl -X POST "$myendpoint/text/analytics/v3.1/languages?'" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: $mykey" --data-ascii "{'documents':[{'id':1,'text':'bonjour'}], 'kind': 'LanguageDetection'}"
+curl -X POST "$AI_SERVICE_ENDPOINT/text/analytics/v3.1/languages?'" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: $AI_SERVICE_KEY" --data-ascii "{'documents':[{'id':1,'text':'bonjour'}], 'kind': 'LanguageDetection'}"
