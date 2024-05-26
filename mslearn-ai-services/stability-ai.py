82a296b03e7df7702b948b28418fd03ce2919228
Liew Chooi Chin <127520432+liewchooichin@users.noreply.github.com>
2024-05-26 14:00:43 +0000

    image-generation

--- mslearn-ai-services/stability-ai.py
+++ mslearn-ai-services/stability-ai.py
@@ -1,59 +1,42 @@
-import urllib.request
-import json
+import requests
+import time
 import os
-import ssl
+from dotenv import load_dotenv
 
-def allowSelfSignedHttps(allowed):
-    # bypass the server certificate verification on client side
-    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
-        ssl._create_default_https_context = ssl._create_unverified_context
+def main(): 
+        
+    try:
+        # Get Azure OpenAI Service settings
+        #load_dotenv()
+        api_base = os.getenv("AZURE_OAI_ENDPOINT")
+        api_key = os.getenv("AZURE_OAI_KEY")
+        api_version = '2024-02-15-preview'
+        
+        # Get prompt for image to be generated
+        prompt = input("\nEnter a prompt to request an image: ")
 
-#allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+        # Call the DALL-E model
+        url = "{}openai/deployments/dalle3/images/generations?api-version={}".format(api_base, api_version)
+        headers= { "api-key": api_key, "Content-Type": "application/json" }
+        body = {
+            "prompt": prompt,
+            "n": 1,
+            "size": "1024x1024"
+        }
+        response = requests.post(url, headers=headers, json=body)
 
-# Request data goes here
-# The example below assumes JSON formatting which may be updated
-# depending on the format your endpoint expects.
-# More information can be found here:
-# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+        # Get the revised prompt and image URL from the response
+        revised_prompt = response.json()['data'][0]['revised_prompt']
+        image_url = response.json()['data'][0]['url']
 
-# My prompt
-# original: "prompt"
-prompt = "A tabby cat jumping over a rainbow."
-data =  {
-  "input_data": {
-    "columns": [
-      prompt,
-      "image",
-      "mask_image"
-    ],
-    "index": [],
-    "data": []
-  },
-  "params": {}
-}
+        # Display the URL for the generated image
+        print(revised_prompt)
+        print(image_url)
+        
 
-body = str.encode(json.dumps(data))
+    except Exception as ex:
+        print(ex)
 
-url = 'https://liewchooichin-7774-jordt.eastus.inference.ml.azure.com/score'
-# Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
-api_key = 'RyXOjXmeFFWvaapI0gOq4kecP6OgrCNQ'
-if not api_key:
-    raise Exception("A key should be provided to invoke the endpoint")
+if __name__ == '__main__': 
+    main()
 
-# The azureml-model-deployment header will force the request to go to a specific deployment.
-# Remove this header to have the request observe the endpoint traffic rules
-headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'stabilityai-stable-diffusion--7' }
-
-req = urllib.request.Request(url, body, headers)
-
-try:
-    response = urllib.request.urlopen(req)
-
-    result = response.read()
-    print(result)
-except urllib.error.HTTPError as error:
-    print("The request failed with status code: " + str(error.code))
-
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-    print(error.info())
-    print(error.read().decode("utf8", 'ignore'))
