bae7e3fac6711d5a88c3f0e0d13ed4bf04a88e45
Liew Chooi Chin <127520432+liewchooichin@users.noreply.github.com>
2024-06-01 10:12:08 +0000

    qna

--- mslearn-ai-language/qna.py
+++ mslearn-ai-language/qna.py
@@ -2,23 +2,37 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+# import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.language.questionanswering import QuestionAnsweringClient
 
 def main():
     try:
         # Get Configuration Settings
-        #load_dotenv()
+        load_dotenv()
         ai_endpoint = os.getenv('LANG_ENDPOINT')
         ai_key = os.getenv('LANG_KEY')
         ai_project_name = os.getenv('QA_PROJECT_NAME')
         ai_deployment_name = os.getenv('QA_DEPLOYMENT_NAME')
 
         # Create client using endpoint and key
-
+        # Create client using endpoint and key
+        credential = AzureKeyCredential(ai_key)
+        ai_client = QuestionAnsweringClient(
+           endpoint=ai_endpoint, credential=credential)
 
         # Submit a question and display the answer
-
-
+        # Submit a question and display the answer
+        user_question = ''
+        while user_question.lower() != 'quit':
+            user_question = input('\nQuestion (enter quit to quit):\n')
+            response = ai_client.get_answers(question=user_question,
+                                     project_name=ai_project_name,
+                                     deployment_name=ai_deployment_name)
+            for candidate in response.answers:
+                print(candidate.answer)
+                print("Confidence: {}".format(candidate.confidence))
+                print("Source: {}".format(candidate.source))
 
     except Exception as ex:
         print(ex)
